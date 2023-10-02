9f23c5240607d933738fa5b7afc78138e7133077
Elkin Torres <elkin.torres@prodigious.com>
2023-10-02 17:32:10 -0500

    Exercise 1 solutions: normal and extra credit

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,19 +3,26 @@
 
 import * as React from 'react'
 
-function Greeting() {
+//*** Normal ***/
+// function Greeting() {
+//*** Extra Credit 1 ***/
+function Greeting(props) {
   // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+  //*** Normal ***/
+  // const [name, setName] = React.useState()
+  //*** Extra Credit 1 ***/
+  const [name, setName] = React.useState(props.initialName)
 
   function handleChange(event) {
     // 🐨 update the name here based on event.target.value
+    setName(event.target.value)
   }
 
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input onChange={handleChange} id="name" value={name} />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
@@ -23,7 +30,10 @@ function Greeting() {
 }
 
 function App() {
-  return <Greeting />
+  //*** Normal ***/
+  // return <Greeting />
+  //*** Extra Credit 1 ***/
+  return <Greeting initialName="stranger" />
 }
 
 export default App
